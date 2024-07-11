8fc114bbe43e1a1ea319d6afba0ad7f082a521f0
Skiferii <dolzhenkoandriy@gmail.com>
2024-07-11 17:42:59 +0200

    Lesson07

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import './App.css';
 
+// Lesson Inport
+import Lesson06 from './lesson/lesson06/Lesson06';
+
+// HomeWork Import
+
+
+
+
 function App() {
   return (
     <div className="App">
-    NEW PROJEKT
+    <Lesson06/>
     </div>
   );
 }
---
+++ src/lesson/lesson06/Lesson06.tsx
@@ -0,0 +1,4 @@
+function Lesson06 () {
+    return <div>Lesson06 - example ts</div>
+}
+export default Lesson06
