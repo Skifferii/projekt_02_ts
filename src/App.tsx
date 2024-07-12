1e1bbd518031e35075cfb5d06b4017e3f199357d
Skiferii <dolzhenkoandriy@gmail.com>
2024-07-12 11:40:46 +0200

    q

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
 import './App.css';
 
 //Lessons import 
-//import Lesson06 from './lessons/lesson06/Lesson06';
-//import Lesson07 from './lessons/lesson07/Lesson07';
+// import Lesson06 from './lessons/Lesson06/Lesson06';
+import Lesson07 from './lessons/Lesson07/Lesson07';
+
 //Homeworks import 
-import HomeWork06 from './homeworks/homework06/HomeWork06';
-//import HomeWork07 from './homeworks/homework07/HomeWork07';
+
 
 function App() {
   return (
     <div className="App">
       {/* <Lesson06 /> */}
-      {/* <Lesson07 /> */}
-
-      
-      <HomeWork06 />
-      {/* <HomeWork07 /> */}
-
-
+      <Lesson07/>
     </div>
   );
 }
