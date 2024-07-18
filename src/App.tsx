569d86534400d9f29cc385eff3e59167a642a632
Skiferii <dolzhenkoandriy@gmail.com>
2024-07-18 07:14:26 +0200

    homework11

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,14 @@ import GlobalStyles from "./styles/GlobalStyles";
 //import Lesson08 from './lessons/Lesson08/Lesson08';
 //import Lesson09 from "./lessons/Lesson09/Lesson09";
 //import Lesson10 from "./lessons/Lesson10/Lesson10";
-import Lesson11 from "./lessons/Lesson11/Lesson11";
+// import Lesson11 from "./lessons/Lesson11/Lesson11";
 //Homeworks import
 //import HomeWork07 from './homeworks/homework07/HomeWork07';
 //import HomeWork08 from './homeworks/homework08/HomeWork08';
 //import HomeWork09 from './homeworks/homework09/HomeWork09';
  //import HomeWork10 from "./homeworks/homework10/Homework10Meine"
 // import HomeWork10 from "./homeworks/homework10/Homework10"
+import HomeWork11 from "./homeworks/homework11/Homework11";
 function App() {
   return (
     <>
@@ -22,7 +23,7 @@ function App() {
       {/* <Lesson08/> */}
       {/* <Lesson09 /> */}
       {/* <Lesson10 /> */}
-      <Lesson11 />
+      {/* <Lesson11 /> */}
       {/* <HomeWork06 /> */}      
       {/* <HomeWork07 /> */}      
       {/* <HomeWork08 /> */}
@@ -30,6 +31,8 @@ function App() {
       {/* <HomeWork10 /> */}
       {/* <HomeWork10/> */}
       {/* <HomeWork10Meine/> */}
+      {/* <HomeWork10/> */}
+      <HomeWork11/>
     </>
   );
 }
